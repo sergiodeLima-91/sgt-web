4cb8eaead73a5fa304103f19f8a3e2bf84523d29
sergio-lima <sergiodelima22@gmail.com>
2024-05-06 10:44:29 -0300

    Inserindo combobox para escolha do tipo de termo

--- sgt-web/src/App.js
+++ sgt-web/src/App.js
@@ -1,11 +1,56 @@
 import './App.css';
+import React, { useState } from 'react';
 import logo from './logo.png'
+
 function App() {
+  const [showOptions, setShowOptions] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const options = [
+    'Adesão',
+    'Encaminhamento',
+    'Declaração',
+    'Cancelamento',
+    'Outro',
+  ];
+
+  const handleArrowClick = () => {
+    setShowOptions(!showOptions);
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setShowOptions(false);
+  };
+
   return (
-    <div>
-      <img src={logo}/>
-      <h1>Sistema Gerador de Termos</h1>
+    <div className='App'>
+      <img src={logo} id='logo'/>
+      {/* Seleção do tipo de termo no combobox: */}
+      <div className="select-container">
+        <div
+          className={`select-arrow ${showOptions ? 'open' : ''}`}
+          onClick={handleArrowClick}
+        >
+        </div>
+        <select
+          className={`term-select ${showOptions ? 'open' : ''}`}
+          value={selectedOption}
+          onChange={(e) => handleOptionSelect(e.target.value)}
+        >
+          <option value="" disable hidden>O que você deseja fazer hoje?</option>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+      {selectedOption && (
+        <p>Você selecionou: <strong>{selectedOption}</strong></p>
+      )}
     </div>
+
   );
 }
 
