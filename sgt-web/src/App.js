eac75ce9f8dc58bd0eb256e6e730cdb45416742f
sergio-lima <sergiodelima22@gmail.com>
2024-05-07 13:49:07 -0300

    Retorno a versao anterior mantendo componentizacao.

--- sgt-web/src/App.js
+++ sgt-web/src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
 import React, { useState } from 'react';
 import logo from './components/logo.png'
-import jsPDF from 'jspdf';
 
 function App() {
   const [showOptions, setShowOptions] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
-  const [tipoTermo, setTipoTermo] = useState('');
-  const [dadosForm, setDadosForm] = useState({
-    // Inicialize os estados para todos os campos necessários
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDadosForm({ ...dadosForm, [name]: value });
-  };
-
-  const gerarPDF = () => {
-    const doc = new jsPDF();
-    doc.text(`Nome da Empresa: ${dadosForm.nomeEmpresa}`, 10, 10);
-    // Adicione mais campos conforme necessário
-    doc.save('termo.pdf');
-  };
 
   const options = [
     'Adesão',
@@ -50,6 +33,7 @@ function App() {
           onClick={handleArrowClick}
         >
         </div>
+        {/* Formulary */}
         <select
           className={`term-select ${showOptions ? 'open' : ''}`}
           value={selectedOption}
@@ -66,31 +50,6 @@ function App() {
       {selectedOption && (
         <p>Você selecionou: <strong>{selectedOption}</strong></p>
       )}
-      {/* TERMO DE ADESÃO */}
-      <div>
-      <select onChange={(e) => setTipoTermo(e.target.value)}>
-        <option value="">Selecione o Tipo de Termo</option>
-        <option value="ADESAO">ADESÃO</option>
-        {/* Adicione mais opções conforme necessário */}
-      </select>
-
-      {tipoTermo === 'ADESAO' && (
-        <>
-          {/* Renderize os campos para ADESÃO */}
-          <input
-            type="text"
-            name="nomeEmpresa"
-            placeholder="Nome da Empresa"
-            onChange={handleChange}
-          />
-          {/* Adicione mais campos de entrada conforme necessário */}
-        </>
-      )}
-
-      {/* Renderize campos adicionais com base no tipoTermo se necessário */}
-
-      <button onClick={gerarPDF}>Gerar Termo</button>
-    </div>
     </div>
 
   );
