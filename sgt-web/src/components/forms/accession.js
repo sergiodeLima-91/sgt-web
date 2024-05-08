a91f6390c66a0ede07fd51bcd7221d7bd088e91d
sergio-lima <sergiodelima22@gmail.com>
2024-05-08 15:36:15 -0300

    Inserindo editor de texto para insercao opcional de contrato ou detalhes do termo

--- sgt-web/src/components/forms/accession.js
+++ sgt-web/src/components/forms/accession.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import jsPDF from 'jspdf';
+import { Editor, EditorState } from 'draft-js';
+import { Page, Text, View, Document, StyleSheet } from '@mikecousins/react-pdf';
+
 
 function Accession() {
-
+  const [editorState, setEditorState] = useState(() => 
+    EditorState.createEmpty()
+  );
   const [companyName, setCompanyName] = useState('');
   
   const gerarPDF = (e) => {
     e.preventDefault();
     const doc = new jsPDF();
-    doc.text(`NOME DA EMPRESA: ${companyName}`, 20,20)
+    doc.text(`NOME DA EMPRESA: ${companyName}`, 20,20);
+
+    const contentState = editorState.getCurrentContent();
+    const contentText = contentState.getPlainText();
+    doc.text(contentText, 20, 40);
 
     return doc.save('termo-de-adesao.pdf')
     }
@@ -24,8 +33,12 @@ function Accession() {
               display:'flex',
               flexDirection:'column'
           }}>
-            <input type='string' name='companyName'placeholder='NOME'value={companyName} onChange={(e) => setCompanyName(e.target.value)} style={{padding:'5px', margin:'10px'}}/>
-            <input type='number' name='cpf-cnpj'placeholder='CPF/CNPJ'style={{padding:'5px', margin:'10px'}}/>
+            <Editor 
+              editorState={editorState}
+              onChange={setEditorState}
+            />
+            <input type='string' name='companyName'placeholder='NOME DA EMPRESA'value={companyName} onChange={(e) => setCompanyName(e.target.value)} style={{padding:'5px', margin:'10px'}}/>
+            <input type='number' name='cnpj'placeholder='CNPJ'style={{padding:'5px', margin:'10px'}}/>
             <input type='string' name='streetch'placeholder='RUA'style={{padding:'5px', margin:'10px'}}/>
             <input type='number' name='numero'placeholder='NÚMERO'style={{padding:'5px', margin:'10px'}}/>
             <button onClick={gerarPDF}>Baixar Termo</button>            
