ffd6588f523f24e21efab9d30fccd8b61bbb8a40
sergio-lima <sergiodelima22@gmail.com>
2024-05-10 15:33:24 -0300

    Revertendo algoritmo para versão anteterior

--- sgt-web/src/components/forms/accession.js
+++ sgt-web/src/components/forms/accession.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import jsPDF from 'jspdf';
 import { Page, Text, View, Document, StyleSheet } from '@mikecousins/react-pdf';
-import { createEditor } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
 
 function Accession() {    
   // Estado do editor de texto:
-  const [editor, setEditor] = useState((withReact(createEditor())));
+  
   // Estado do formulário:
   const [companyName, setCompanyName] = useState('');
   
@@ -29,17 +27,7 @@ function Accession() {
               display:'flex',
               flexDirection:'column'
           }}>
-            {/* Editor de texto vai aqui */}
-          <Slate editor={editor}>
-            <Editable 
-              placeholder='Conteúdo do contrato'
-              onKeyDown={(event => {
-                 console.log('Tecla pressionada:', event.key)
-              })}
-
-            />
-
-          </Slate>
+            {/* Editor de texto vai aqui */}          
 
             <input type='string' name='companyName'placeholder='NOME DA EMPRESA'value={companyName} onChange={(e) => setCompanyName(e.target.value)} style={{padding:'5px', margin:'10px'}}/>
             <input type='number' name='cnpj'placeholder='CNPJ'style={{padding:'5px', margin:'10px'}}/>
