9934d9a4412ebea7d6afd25045f737c410ac1d35
sergio-lima <sergiodelima22@gmail.com>
2024-05-20 15:24:05 -0300

    Adequando projeto a linguagem Typescript

---
+++ sgt-web/src/components/TextEditor.ts
@@ -0,0 +1,30 @@
+import { type } from '@testing-library/user-event/dist/type';
+import React, { useState } from 'react';
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+import { BaseEditor, Descendant } from 'slate'
+import { ReactEditor } from 'slate-react'
+
+
+function TextEditor() {
+  const [editor] = useState(() => withReact(createEditor()))
+
+  type CustomElement = { type: 'paragraph'; children: CustomText[] }
+  type CustomText = { text: string }
+  
+  declare module 'slate' {
+    interface CustomTypes {
+      Editor: BaseEditor & ReactEditor
+      Element: CustomElement
+      Text: CustomText
+    }
+  }
+
+  return(
+    <>
+      
+    </>
+  )
+}
+
+export default TextEditor
