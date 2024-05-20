a30e8847ce35a4d6698cac3f87486cddb1f9db7a
sergio-lima <sergiodelima22@gmail.com>
2024-05-20 10:44:21 -0300

    Procurando outra biblioteca para gerar editor de texto

--- sgt-web/src/components/TextEditor.js
+++ sgt-web/src/components/TextEditor.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-import draftToHtml from 'draftjs-to-html';
 
 
- export class TextEditor extends Component {
-  state = {
-    editorState: EditorState.createEmpty(),
-  }
-
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  render() {
-    const { editorState } = this.state;
-    return (
-      <div>
-        <Editor
-          editorState={editorState}
-          wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor"
-          onEditorStateChange={this.onEditorStateChange}
-        />
-        <textarea
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        />
-      </div>
-    );
-  }
+function TextEditor() {
+  return(
+    <>
+      <p>Editor de Texto vai aqui</p>
+    </>
+  )
 }
 
 export default TextEditor
